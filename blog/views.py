f64616ca5c262df6cec00f88d350529a5674b6a7
oriya hazan <oriyaha@ac.sce.ac.il>
2022-12-11 17:45:15 +0200

    first commit

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+def home(request):
+    context= {
+        'posts' : Post.objects.all()
+    }
+    return render(request , 'blog/HomePage.html' , context)
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
+
+
